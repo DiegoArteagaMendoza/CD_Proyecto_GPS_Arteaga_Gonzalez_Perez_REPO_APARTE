fa90b7e4af5c7365e90279a581d12b55e3f87c30
DiegoArteagaMendoza <daarteagamendoza@gmail.com>
2025-06-01 20:17:53 -0400

    versiones y monitoreo

--- port-forward-all.ps1
+++ port-forward-all.ps1
@@ -1,13 +1,13 @@
 # Lista de servicios y sus puertos
 $services = @{
-    "ms-farmacia"           = 8006;
-    "ms-inventario-svc"     = 8002;
-    "ms-trabajador"         = 8003;
-    "ms-usuariocliente"     = 8004;
-    "ms-venta"              = 8005;
+    "ms-farmacia"       = 8006;
+    "ms-inventario-svc" = 8002;
+    "ms-trabajador"     = 8003;
+    "ms-usuariocliente" = 8004;
+    "ms-venta"          = 8005;
 }
 
-# Namespace donde están desplegados
+# Namespace donde están desplegados los microservicios principales
 $namespace = "backend"
 
 # Iterar y hacer port-forward para cada servicio
@@ -17,17 +17,20 @@ foreach ($service in $services.GetEnumerator()) {
 
     Write-Host "Iniciando port-forward para $svcName en puerto $port..." -ForegroundColor Green
 
-    # Usamos ${} para asegurar que PowerShell interprete bien la variable
     Start-Process -NoNewWindow powershell -ArgumentList "kubectl port-forward svc/$svcName `${port}:`${port} -n $namespace"
     
-    # Esperar un poco para que se establezca la conexión
     Start-Sleep -Seconds 3
-
-    # Opcional: abrir en el navegador
-    # $url = "http://localhost:$port"
-    # Write-Host "Abriendo $url en el navegador..." -ForegroundColor Cyan
-    # Start-Process $url
 }
+
+# Port-forward para servicios de monitoring
+Write-Host "Iniciando port-forward para grafana-service en puerto 3000..." -ForegroundColor Cyan
+Start-Process -NoNewWindow powershell -ArgumentList "kubectl port-forward svc/grafana-service 3000:3000 -n monitoring"
+Start-Sleep -Seconds 3
+
+Write-Host "Iniciando port-forward para prometheus-service en puerto 9090..." -ForegroundColor Cyan
+Start-Process -NoNewWindow powershell -ArgumentList "kubectl port-forward svc/prometheus-service 9090:9090 -n monitoring"
+Start-Sleep -Seconds 3
+
 
 # comandos de ejecucion en powershell
 # Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
